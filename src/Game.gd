652df2fee6ccd2834d3f4ecdcbb009e835a16427
Goniix <dragonixgold@gmail.com>
2024-05-01 08:07:06 +0200

    updated hierarchy

--- src/Game.gd
+++ src/Game.gd
@@ -1,9 +1,13 @@
 class_name Game
 extends Node
 
+var cardScene : PackedScene = load("res://scenes/card.tscn")
 
 func _init():
-	pass
+	var new_card = cardScene.instantiate()
+	new_card.position = Vector2(0,200)
+	new_card.get_node("Background").texture = load("res://spr/frames/frame_common_beast.png")
+	add_child(new_card)
 	
 func _process(delta):
 	pass
