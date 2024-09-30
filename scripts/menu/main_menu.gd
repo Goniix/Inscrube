247ca7db103ed2d683f555b7c98e865589d6b8ae
Goniix <dragonixgold@gmail.com>
2024-09-30 07:44:34 +0200

    started collection

--- scripts/menu/main_menu.gd
+++ scripts/menu/main_menu.gd
@@ -1,5 +1,7 @@
 extends Node
 
+@export var settingsPath: NodePath = "res://scenes/menu/settings_menu.tscn"
+@export var mainPath: NodePath = "res://scenes/main.tscn"
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -16,8 +18,8 @@ func _on_quit_button_pressed() -> void:
 
 
 func _on_play_button_pressed() -> void:
-	get_tree().change_scene_to_file("res://scenes/main.tscn")
+	get_tree().change_scene_to_file(mainPath)
 
 
 func _on_settings_button_pressed() -> void:
-	get_tree().change_scene_to_file("res://scenes/settings_menu.tscn")
+	get_tree().change_scene_to_file(settingsPath)
--- scripts/networker.gd
+++ scripts/networker.gd
@@ -1,15 +1,23 @@
 extends Node
 
+# enum CLIENT_STATES {WAITING,RUNNING}
+# var client_state: CLIENT_STATES 
+
 var player_list: Array = []
 var socket: ENetMultiplayerPeer
 const DEFAULT_PORT = 1234
 const LOOPBACK = "127.0.0.1"
 
+# enum ANSWERS {AFFIRMATIVE,NEGATIVE}
+
 func slog(text:String):
-	if multiplayer.is_server():
-		print("[SERVER]: "+text)
+	if socket.get_connection_status() == 2:
+		if multiplayer.is_server():
+			print("[SERVER]: "+text)
+		else:
+			print("["+str(multiplayer.get_unique_id())+"]: "+text)
 	else:
-		print("["+str(multiplayer.get_unique_id())+"]: "+text)
+		print("[DISCONNECTED_CLIENT]: "+text)
 
 func start_host():
 	socket = ENetMultiplayerPeer.new()
@@ -40,6 +48,10 @@ func _on_new_peer_connection(id:int):
 		slog("New client connected with ID:"+str(id))
 		slog("Peer count is now "+str(multiplayer.get_peers().size()))
 
+		await get_tree().create_timer(5).timeout
+
+		multiplayer.disconnect_peer(id)
+
 	else: #is client
 		pass
 
@@ -50,9 +62,13 @@ func _on_peer_disconnection(id:int):
 	else: #is client
 		pass
 		
+#SERVER ANSWERS
 
+# func answer_client(answer:ANSWERS):
+# 	pass
 
 func _ready() -> void:
+	# process_mode = Node.PROCESS_MODE_ALWAYS
 	multiplayer.connected_to_server.connect(_on_successful_server_connection)
 	multiplayer.connection_failed.connect(_on_failed_server_connection)
 	multiplayer.peer_connected.connect(_on_new_peer_connection)
