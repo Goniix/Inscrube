c0be8d2b27ba0c93cc49ad367c3834b016dfb9cf
Gabriel Redouin-Innecco <dragonixgold@gmail.com>
2024-10-03 16:57:35 +0200

    deck save

--- data/deck_data.gd
+++ data/deck_data.gd
@@ -4,20 +4,24 @@ var name: String
 var card_list: Array[CardData] = []
 
 
-func _init(_name:String= "NAMELESS"):
-    self.name = _name
+func _init(_name:String= ""):
+	self.name = _name
 
 func _to_string():
-    return "Deck("+name+")"
+	return "Deck("+name+")"
 
 func toJSON()-> Dictionary:
-    var res_dict = {
-        "name" = name,
-        "card_list" = card_list
-    }
-    return res_dict
+	var card_list_copy: Array = []
+	card_list_copy.append(card_list)
+	var res_dict = {
+		"name" = name,
+		"card_list" = card_list
+	}
+	return res_dict
 
 static func fromJSON(json_data:Dictionary)->DeckData:
-    var res: DeckData = DeckData.new(json_data["name"])
-    res.card_list = json_data["card_list"]
-    return res
+	var res: DeckData = DeckData.new(json_data["name"])
+	for card in json_data["card_list"]:
+		res.card_list.append(card)
+
+	return res
--- scripts/menu/collection_menu.gd
+++ scripts/menu/collection_menu.gd
@@ -4,7 +4,8 @@ signal FinishedThreadCardCache(card_texture:Texture2D)
 
 var card_scene : PackedScene = preload("res://scenes/card.tscn")
 var deck_list : Dictionary = {}
-var selected_deck : DeckListButton = null
+# var selected_deck : DeckListButton = null
+var edit_deck_list: Array[CardData] = []
 
 func get_card_texture(card_name:String, file_cache: bool = false):
 	var card:Card = card_scene.instantiate()
@@ -22,13 +23,15 @@ func get_card_texture(card_name:String, file_cache: bool = false):
 	# card_textures.append(card_texture)
 	if file_cache:
 		var dir = DirAccess.open("user://")
-		if dir.dir_exists("cache"):
-			for file in DirAccess.get_files_at("user://cache/"):  
-				DirAccess.remove_absolute(file)
-			dir.remove("cache")
-		dir.make_dir("cache")
-		
-		card_texture.get_image().save_png("user://cache/"+card_name.to_lower()+".png")
+		if !dir.dir_exists("cache"):
+			dir.make_dir("cache")
+			# for file in DirAccess.get_files_at("user://cache/"):  
+			# 	DirAccess.remove_absolute(file)
+			# dir.remove("cache")
+		var cache_name: String = "user://cache/"+card_name.to_lower()+".png"
+		if !FileAccess.file_exists(cache_name):
+			card_texture.get_image().save_png(cache_name)
+
 	remove_child(card)
 	card.queue_free()
 
@@ -53,11 +56,15 @@ func save_deck(_deck_data:DeckData):
 	# 	print(error)
 
 func load_deck(_path:String):
-	var file = FileAccess.open(_path,FileAccess.READ)
-	var json = JSON.new()
-	json.parse(file.get_line())
-	var json_data: Dictionary = json.get_data()
+	var file: FileAccess = FileAccess.open(_path,FileAccess.READ)
+	var json_data: Dictionary = JSON.parse_string(file.get_line())
+	file.close()
 	add_deck(DeckData.fromJSON(json_data))
+
+func is_deck_name_valid(deck_name:String):
+	if FileAccess.file_exists("user://decks/"+deck_name+".adck"):
+		return false
+	return true
 
 func _init():
 	# FinishedThreadCardCache.connect(_on_finished_thread_card_cache)
@@ -67,11 +74,19 @@ func _init():
 func _ready() -> void:
 	RessourceManager.loadAllCards()	
 	for card_name in RessourceManager.cardData.keys():
-		await get_card_texture(card_name,true)
+		await get_card_texture(card_name)
 	
-	add_deck(DeckData.new())
-	
-	
+	# add_deck(DeckData.new("Test"))
+	reload_deck_list()
+
+func reload_deck_list():
+	for child in $DeckList/VBoxContainer.get_children():
+		$DeckList/VBoxContainer.remove_child(child)
+		child.queue_free()
+		
+	deck_list.clear()
+	for file in DirAccess.get_files_at("user://decks/"):
+		load_deck("user://decks/"+file)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
@@ -81,14 +96,18 @@ func _process(delta: float) -> void:
 func _on_back_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/menu/main_menu.tscn")
 
-# func _on_finished_thread_card_cache(card_texture: Texture2D):
-# 	var cardList: ItemList = get_node("CardList")
-# 	cardList.add_icon_item(card_texture)
 
 func _on_deck_button_pressed(button:DeckListButton):
-	selected_deck = button
+	var data = button.deck_data
+	edit_deck_list = data.card_list.duplicate()
+
 
 func _on_save_button_pressed() -> void:
-	var deck: DeckData = deck_list[selected_deck]
+	var deck: DeckData = DeckData.new(%DeckNameInput.text)
+	deck.card_list = edit_deck_list
 	save_deck(deck)
 	print("Saved deck! "+str(deck))
+	reload_deck_list()
+
+func _on_deck_name_input_text_changed(new_text:String) -> void:
+	%SaveDeckButton.disabled = !is_deck_name_valid(new_text)
